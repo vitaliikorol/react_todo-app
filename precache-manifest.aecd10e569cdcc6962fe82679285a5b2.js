7022f688b0a06d77136b46839c964409bc14b283
Vitalii Korol <vitaliikorol@192-168-0-105.rdsnet.ro>
2020-05-07 10:55:53 +0300

    Updates

--- precache-manifest.aecd10e569cdcc6962fe82679285a5b2.js
+++ precache-manifest.aecd10e569cdcc6962fe82679285a5b2.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e514590a6ce2444b1f564b714753cb9f",
+    "revision": "a0edb99b29e06beab3d39ccb37a8ee64",
     "url": "./index.html"
   },
   {
-    "revision": "dc705410501abc4f7a32",
+    "revision": "f73e3add2e804260465f",
     "url": "./static/css/main.e56c2ea1.chunk.css"
   },
   {
-    "revision": "66caa476510d497d7adb",
-    "url": "./static/js/2.1fb9bdff.chunk.js"
+    "revision": "fa96609b41159d6a0fb8",
+    "url": "./static/js/2.cfc470f9.chunk.js"
   },
   {
     "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
-    "url": "./static/js/2.1fb9bdff.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.cfc470f9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "dc705410501abc4f7a32",
-    "url": "./static/js/main.5bcac892.chunk.js"
+    "revision": "f73e3add2e804260465f",
+    "url": "./static/js/main.a1cfc4f0.chunk.js"
   },
   {
     "revision": "548aac5e4720dc7e1ea8",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.c4a02aab128eba15434c4e34231fbc22.js"
+  "./precache-manifest.aecd10e569cdcc6962fe82679285a5b2.js"
 );
 
 self.addEventListener('message', (event) => {
